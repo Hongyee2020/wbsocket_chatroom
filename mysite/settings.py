2674019ec268c5268648756cd38f4e360628561a
liqingjun <lqj@qq.com>
2025-11-30 17:01:11 +0800

    时区

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,9 +134,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'  # 简体中文
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'  # 上海时间
 
 USE_I18N = True
 
